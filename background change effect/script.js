dea9f2bd70e6b25f85948aa5ee7de1b81e6bed32
JosipSudar <josip.sudar@gmail.com>
2024-01-31 10:16:35 +0100

    31/01/2024

---
+++ background change effect/script.js
@@ -0,0 +1,13 @@
+let imgBox = document.querySelector(".img-box");
+let imgWrap = document.querySelector(".img-wrap");
+let originalImg = document.getElementById("originalImg");
+let line = document.getElementById("line");
+let leftSpace = imgBox.offsetLeft;
+
+originalImg.style.width = imgBox.offsetWidth + "px";
+
+imgBox.onmousemove = (e) => {
+  let boxWidth = e.pageX - leftSpace + "px";
+  imgWrap.style.width = boxWidth;
+  line.style.left = boxWidth;
+};
