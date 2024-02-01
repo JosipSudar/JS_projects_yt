29f1fec3d4372b95f5846b58687c28682e3c6d6f
JosipSudar <josip.sudar@gmail.com>
2024-02-01 15:34:03 +0100

    _2

---
+++ crypto website/script.js
@@ -0,0 +1,15 @@
+let btc = document.getElementById("bitcoin");
+let eth = document.getElementById("ethereum");
+let doge = document.getElementById("dogecoin");
+let env = {
+  async: true,
+  scrossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  method: "GET",
+  headers: {},
+};
+$.ajax(env).done(function (response) {
+  btc.innerHTML = response.bitcoin.usd;
+  eth.innerHTML = response.ethereum.usd;
+  doge.innerHTML = response.dogecoin.usd;
+});
